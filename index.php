7168d50d922689e26ec970ef276b964fa2d14f84
wodniak12 <72550912+wodniak12@users.noreply.github.com>
2020-12-03 12:43:55 +0100

    Plemie

--- index.php
+++ index.php
@@ -54,72 +54,46 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
-<body style="background-color:   rgb(202, 115, 0);">
-<header style="background-color:  rgb(109, 58, 0);">
-	
-    <nav class="navbar navbar-expand-lg navbar- rgb(202, 115, 0) bg- rgb(202, 115, 0)" >
-    
+<body style="background-color:    rgb(70, 31, 0);">
+<header style="background-color:  rgb(70, 31, 0);">
+
+  <button style="border-width: 10px; border-style: double; border-color: rgb(24, 11, 0); margin-left: 50px; margin-top: 50px;" type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+  Gracz
+  </button>
+  <div class="dropdown-menu p-4 text-muted" style="max-width: 200px;">
+  <a class="dropdown-item" href="#">Nick:</a>
+    <a class="dropdown-item" href="#">Gildia:</a> 
+    <a class="dropdown-item" href="#">Level:</a> 
+    <button type="button" class="btn btn-outline-warning">Wyloguj</button>
+  
+</div>
+</div>
+
+
+        
         
 
-    
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainmenu" aria-controls="mainmenu" aria-expanded="false" aria-label="Przełącznik nawigacji">
-            Ekwipunek
-        </button>
-        
-
-        <div class="collapse navbar-collapse" id="mainmenu" >
-        
-            <ul class="navbar-nav mt-2 mt-lg-0 mx-auto ">
-            
-                <li class="nav-item active" style="margin-right: 50px;" >
-    <button class="btn btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-    Surowce
+<div class="contener" style=" float: right; margin-right: 50px; margin-top: 50px; ">
+<div class="dropdown" role="group" aria-label="Basic example" >
+    <button style="border-width: 10px; border-style: double; border-color: rgb(24, 11, 0);" class="btn btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample5" aria-expanded="false" aria-controls="collapseExample5">
+    Magazyn
   </button>
 </p>
-<div class="collapse" id="collapseExample">
-  <div class="card card-body">
+<div class="collapse" id="collapseExample5">
+<div class="card card-body">
   <a class="dropdown-item" href="#">Drewno: <?php echo $v->showStorage("wood"); ?></a> 
   <a class="dropdown-item" href="#">Żelazo: <?php echo $v->showStorage("iron"); ?></a>
   <a class="dropdown-item" href="#">Żniwa: <?php echo $v->showStorage("food"); ?></a>
   </div>
 </div>
-                
-                </li>
-                
-                
-                 
-                
-                <br>
-
-
-
-                
-                
-                <li class="nav-item" style="margin-right: 50px;">
-                <button class="btn btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">
-    Tartak
-  </button>
-</p>
-<div class="collapse" id="collapseExample2">
-  <div class="card card-body">
-  Drwal,<br>
-         poziom <?php echo $v->buildingLVL("woodcutter"); ?> <br>
-        Zysk/h: <?php echo $v->showHourGain("wood"); ?><br>
-        <a href="index.php?action=upgradeBuilding&building=woodcutter">
-            <button type="button" class="btn btn-outline-warning">Rozbuduj drwala</button>
-        </a>
-  </div>
-</div>
-
-                </li>
-                <br>
-                <li class="nav-item" style="margin-right: 50px;">
-                <button class="btn btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample3">
+<br>
+<div class="dropdown" role="group" aria-label="Basic example" >
+    <button style="border-width: 10px; border-style: double; border-color: rgb(24, 11, 0);" class="btn btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample3">
     Kopalnia
   </button>
 </p>
 <div class="collapse" id="collapseExample3">
-  <div class="card card-body">
+<div class="card card-body">
   Kopalnia żelaza,<br>
          poziom <?php echo $v->buildingLVL("ironMine"); ?> <br>
         Zysk/h: <?php echo $v->showHourGain("iron"); ?><br>
@@ -128,17 +102,30 @@
         </a>
         </div>
 </div>
-                
-                </li>
-
-                <br>
-                <li class="nav-item" style="margin-right: 50px;">
-                <button class="btn btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample4" aria-expanded="false" aria-controls="collapseExample4">
-                Pole
+<br>
+<div class="dropdown" role="group" aria-label="Basic example" >
+    <button style="border-width: 10px; border-style: double; border-color: rgb(24, 11, 0);" class="btn btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample4" aria-expanded="false" aria-controls="collapseExample4">
+    Tartak
   </button>
 </p>
 <div class="collapse" id="collapseExample4">
-  <div class="card card-body">
+<div class="card card-body">
+  Drwal,<br>
+         poziom <?php echo $v->buildingLVL("woodcutter"); ?> <br>
+        Zysk/h: <?php echo $v->showHourGain("wood"); ?><br>
+        <a href="index.php?action=upgradeBuilding&building=woodcutter">
+            <button type="button" class="btn btn-outline-warning">Rozbuduj drwala</button>
+        </a>
+  </div>
+</div>
+<br>
+<div class="dropdown" role="group" aria-label="Basic example" >
+    <button style="border-width: 10px; border-style: double; border-color: rgb(24, 11, 0);" class="btn btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">
+    Pole
+  </button>
+</p>
+<div class="collapse" id="collapseExample2">
+<div class="card card-body">
   Rozbudowa pola,<br>
          poziom <?php echo $v->buildingLVL("foodearth"); ?> <br>
         Zysk/h: <?php echo $v->showHourGain("food"); ?><br>
@@ -147,41 +134,15 @@
         </a>
         </div>
 </div>
-               
-                </li>
-                <br>
-                <li class="nav-item" style="margin-right: 50px;">
-                
-                </li>
-                <br>
-                
-            
-            </ul>
-            
-        </div>
-        <br>
-        <div class="btn-group-vertical" >
-    <button class="btn btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample1">
-    Gracz
-  </button>
-</p>
-<div class="collapse" id="collapseExample1">
-  <div class="card card-body">
-  <a class="dropdown-item" href="#">Nick:</a>
-    <a class="dropdown-item" href="#">Gildia:</a> 
-    <a class="dropdown-item" href="#">Level:</a> 
-    <button type="button" class="btn btn-outline-warning">Wyloguj</button>
-    </div>
 </div>
-        
 
-    </nav>
+    
     
     
 </header>
 
 
-  <img src="plemiona1.png" alt="wioska" style=" margin-left: auto; margin-right: auto; margin-top: 100px;">
+  <img src="plemiona1.png" alt="wioska" style=" margin-left: auto; margin-right: auto; margin-top: 100px; border-width: 10px; border-style: double; border-color: rgb(24, 11, 0);">
 
     
 
